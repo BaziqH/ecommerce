474e3417362fc125dd66f79675ab8f82bb97f35a
BaziqH <baziq100@gmail.com>
2026-03-31 11:04:22 +0500

    Initial Commit

---
+++ ecommerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ecommerce
+//
+//  Created by Baziq Hameed on 31/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
